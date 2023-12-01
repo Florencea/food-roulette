3ec656bbaaf56310f3ab47084a8e2d0b23a1e232
Florencea <bearflorencea@gmail.com>
2023-12-01 23:19:16 +0800

    chore: add auto upgrade ci

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 // @ts-check
 
+const isDev = !(process.env.NODE_ENV === "production");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/food-roulette",
-  reactStrictMode: true,
   output: "export",
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: !isDev,
+  },
+  eslint: {
+    ignoreDuringBuilds: !isDev,
+  },
+  typescript: {
+    ignoreBuildErrors: !isDev,
+  },
 };
 
 module.exports = nextConfig;
