1725d295e4565b7dda4778814bb7f86e599554f4
Florencea <bearflorencea@gmail.com>
2024-03-28 15:51:24 +0800

    test: localhost access

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { data } from "./data";
 
 export default function App() {
   const [idx, setIdx] = useState(0);
+  useEffect(() => {
+    const f = async () => {
+      const res = await fetch("http://localhost:5173");
+      const data = await res.text();
+      console.log(data);
+    };
+    f();
+  }, []);
   return (
     <main
       onClick={() => {
